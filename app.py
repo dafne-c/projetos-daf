1892c4bfc745472d3ca14cac84683412fff28386
Dafne <caroldaphny@gmail.com>
2026-03-26 12:53:45 +0000

    .

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ def index():
 
 @app.route("/contato")
 def contato():
-    return render_template('contato.html')
+    dados = {'nome' : 'dafne', 'email' : 'c.dafne@escolar.ifrn.edu.br'}
+    return render_template('contato.html', dados=dados)
 
 @app.route("/disciplinas")
 def disciplinas():
@@ -17,6 +18,18 @@ def disciplinas():
 @app.route("/curriculo")
 def curriculo():
     return render_template('curriculo.html')
+
+@app.route('/usuario', defaults={'nome' : 'desconhecido', 'sobrenome' : 'desconhecido'})
+@app.route('/usuario/<nome>/<sobrenome>/')
+def usuario(nome, sobrenome):
+    info = {'nome' : nome, 'sobrenome' : sobrenome}
+    return render_template('usuario.html', info=info)
+
+@app.route('/semestre/<int:x>')
+def semestre(x):
+    atual = x
+    anterior = x - 1
+    return render_template('semestre.html', atual=atual, anterior=anterior)
 
 if __name__ == '__main__':
     app.run()
