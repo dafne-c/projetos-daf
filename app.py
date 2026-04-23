bea6884a5554dacff4dfa27060e3a40fa3988bd2
Dafne <caroldaphny@gmail.com>
2026-04-23 10:59:39 +0000

    por enquanto tá assim

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -30,6 +30,28 @@ def semestre(x):
     atual = x
     anterior = x - 1
     return render_template('semestre.html', atual=atual, anterior=anterior)
+
+@app.route('/dados')
+def dados():
+    return render_template('dados.html')
+
+@app.route('/recebedados')
+def recebedados():
+    nome = request.args.get('nome')
+    sobrenome = request.args.get('sobrenome')
+    email = request.args.get('email')
+    return render_template('recebedados.html', nome=nome, sobrenome=sobrenome, email=email)
+
+@app.route('/verificaridade/<int:idade>')
+def verificaridade(idade):
+    if idade >= 18:
+        return 'você é maior de idade'
+    else:
+        return 'você é menor de idade'
+
+@app.route('/verificaridade2/<int:idade>')
+def verificaridade2(idade):
+    return render_template('idade.html', idade=idade)
 
 if __name__ == '__main__':
     app.run()
